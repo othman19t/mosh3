9f87b3e210560017833ed5e9727d087273148bc4
othman19t <othman19t@gmail.com>
2020-06-24 15:11:15 -0700

    2) adding actions

--- src/store/auth.js
+++ src/store/auth.js
@@ -2,3 +2,22 @@
 const BUG_ADDED = "BUG_ADDED";
 const BUG_REMOVED = "BUG_REMOVED";
 const BUG_RESOLVED = "BUG_RESOLVED";
+
+// 2) adding actions
+export const bugAdded = (description) => ({
+  type: BUG_ADDED,
+  payload: {
+    description,
+    resolved: false,
+  },
+});
+
+export const bugRemoved = (id) => ({
+  type: BUG_REMOVED,
+  payload: { id },
+});
+
+export const bugResolved = (id) => ({
+  type: BUG_RESOLVED,
+  payload: { id },
+});
