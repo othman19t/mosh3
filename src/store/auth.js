3cb9e577ef2e4e52973836a0252658e88be6a677
othman19t <othman19t@gmail.com>
2020-06-24 15:42:35 -0700

    3) adding reducer

--- src/store/auth.js
+++ src/store/auth.js
@@ -21,3 +21,28 @@ export const bugResolved = (id) => ({
   type: BUG_RESOLVED,
   payload: { id },
 });
+
+// 3) adding reducer
+let lastId = 0;
+export const reducer = (state = [], action) => {
+  switch (action.type) {
+    case BUG_ADDED:
+      return [
+        ...state,
+        {
+          id: lastId + 1,
+          description: action.payload.description,
+          resolved: false,
+        },
+      ];
+    case BUG_REMOVED:
+      return state.filter((bug) => bug.id === action.payload.id);
+
+    case BUG_RESOLVED:
+      return state.map((bug) =>
+        bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
+      );
+  }
+
+  return state;
+};
